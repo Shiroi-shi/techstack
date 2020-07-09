1c894fefeebd2102aee4dfa6573ad3ce92e44b03
Aurélien Toussaint <shiroi-shi@live.fr>
2020-07-09 16:45:22 +0200

    add basic functions and order data.json alphabetically

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const data = require ('./data/data.json');
+const data = require('./data/data.json');
 
-console.log(data);
+const languages = data.filter(function (el) {
+  return el.type === 'Language';
+});
+
+const frameworks = data.filter(function (el) {
+  return el.type === 'Framework';
+});
+
+const libraries = data.filter(function (el) {
+  return el.type === 'Library';
+});
+
+const tools = data.filter(function (el) {
+  return el.type === 'Tool';
+});
+
+const all = data;
+
+module.exports = {all, frameworks, languages, libraries, tools};
